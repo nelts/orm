8f26c524bbd0cb22ef12b2a62aabf9050e5ce778
evio <evio@vip.qq.com>
2019-06-27 20:24:02 +0800

    ts compile

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,11 @@
 import 'reflect-metadata';
 import * as sequelize from 'sequelize';
+export interface CacheableInterface {
+    set(pathParams?: object, expire?: number): Promise<any>;
+    get(pathParams?: object, expire?: number): Promise<any>;
+    delete(pathParams?: object): Promise<void>;
+    invoke(): Promise<any>;
+}
 export declare function Cacheable(path: string): (target: any, property: string, descriptor: PropertyDescriptor) => void;
 declare type sequelizeFieldValues = {
     dataValues: object;
