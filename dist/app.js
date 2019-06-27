8438b8c9b5ec5d383bacd537d4d103ccd6a583b1
evio <evio@vip.qq.com>
2019-06-27 19:15:01 +0800

    use transation to create tables

--- dist/app.js
+++ dist/app.js
@@ -19,7 +19,7 @@ exports.default = (app) => {
         ctx.sequelize = Object.freeze(sequelizes);
         ctx.redis = redis;
     });
-    app.on('props', configs => {
+    app.on('props', async (configs) => {
         if (configs.sequelize) {
             const sequelizeConfigs = !Array.isArray(configs.sequelize)
                 ? [configs.sequelize]
@@ -37,12 +37,20 @@ exports.default = (app) => {
                 if (temp[config.alias]) {
                     if (!sequelizes[config.alias])
                         sequelizes[config.alias] = {};
+                    const t = await sequelize.transaction();
+                    const transates = [];
                     for (const table in temp[config.alias]) {
                         const expo = temp[config.alias][table];
-                        if (expo.installer)
+                        if (expo.installer) {
                             expo.installer(sequelize);
+                            transates.push(expo.sync({ transaction: t }));
+                        }
                         sequelizes[config.alias][table] = expo;
                     }
+                    await Promise.all(transates).then(() => t.commit()).catch(e => {
+                        console.error(e);
+                        return t.rollback();
+                    });
                     sequelizes[config.alias] = Object.freeze(sequelizes[config.alias]);
                 }
             }
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { Sequelize, PoolOptions, RetryOptions, Dialect, Model } from 'sequelize'
 import globby from 'globby';
 import * as Redis from 'ioredis';
 import RedisJSON from './redis';
+import { Transaction } from 'sequelize';
 
 type SequelizeMap<T = any> = { 
   [name: string]: {
@@ -27,7 +28,7 @@ export default (app: Plugin) => {
     ctx.redis = redis;
   });
 
-  app.on('props', configs => {
+  app.on('props', async configs => {
     if (configs.sequelize) {
       const sequelizeConfigs = !Array.isArray(configs.sequelize) 
         ? [configs.sequelize] 
@@ -46,11 +47,20 @@ export default (app: Plugin) => {
 
         if (temp[config.alias]) {
           if (!sequelizes[config.alias]) sequelizes[config.alias] = {};
+          const t = await sequelize.transaction();
+          const transates: Promise<any>[] = [];
           for (const table in temp[config.alias]) {
             const expo = temp[config.alias][table];
-            if (expo.installer) expo.installer(sequelize);
+            if (expo.installer) {
+              expo.installer(sequelize);
+              transates.push(expo.sync({ transaction: t }));
+            }
             sequelizes[config.alias][table] = expo;
           }
+          await Promise.all(transates).then(() => t.commit()).catch(e => {
+            console.error(e);
+            return t.rollback();
+          });
           sequelizes[config.alias] = Object.freeze(sequelizes[config.alias]);
         }
       }
